690888e2908318f6344579b8bcb8f6fcdeceabc1
Phong Võ <phong.vo@mylangroup.com>
2026-09-17 10:06:37 +0700

    [v1.2.1] - [FastMapper]: Add MapList, CopyDynamic, MapDynamic, and MapListDynamic compiled delegates

--- src/ZeroPrimitives.Core/Mapping/FastMapper.cs
+++ src/ZeroPrimitives.Core/Mapping/FastMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -13,6 +14,9 @@ namespace ZeroPrimitives.Mapping
     {
         private static readonly ConcurrentDictionary<(Type, Type), Delegate> CopierCache =
             new ConcurrentDictionary<(Type, Type), Delegate>();
+
+        private static readonly ConcurrentDictionary<(Type, Type), Action<object, object>> DynamicCopierCache =
+            new ConcurrentDictionary<(Type, Type), Action<object, object>>();
 
         /// <summary>
         /// Copies matching public properties from source to target.
@@ -47,6 +51,106 @@ namespace ZeroPrimitives.Mapping
             return target;
         }
 
+        /// <summary>
+        /// Maps a collection of TSource to a List of TTarget using compiled IL delegates.
+        /// </summary>
+        public static List<TTarget> MapList<TSource, TTarget>(IEnumerable<TSource>? sourceList)
+            where TSource : class
+            where TTarget : class, new()
+        {
+            var result = new List<TTarget>();
+            if (sourceList == null) return result;
+
+            var key = (typeof(TSource), typeof(TTarget));
+            if (!CopierCache.TryGetValue(key, out var del))
+            {
+                del = CreateCopier<TSource, TTarget>();
+                CopierCache[key] = del;
+            }
+
+            var copier = (Action<TSource, TTarget>)del;
+            foreach (var item in sourceList)
+            {
+                if (item == null) continue;
+                var target = new TTarget();
+                copier(item, target);
+                result.Add(target);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Dynamically copies matching properties from source (untyped object) to target using cached compiled expressions.
+        /// </summary>
+        public static TTarget? CopyDynamic<TTarget>(object? source, TTarget? target)
+            where TTarget : class
+        {
+            if (source == null || target == null) return target;
+
+            var sourceType = source.GetType();
+            var targetType = typeof(TTarget);
+            var key = (sourceType, targetType);
+
+            if (!DynamicCopierCache.TryGetValue(key, out var copier))
+            {
+                copier = CreateDynamicCopier(sourceType, targetType);
+                DynamicCopierCache[key] = copier;
+            }
+
+            copier(source, target);
+            return target;
+        }
+
+        /// <summary>
+        /// Dynamically maps untyped object to a new instance of TTarget using cached compiled expressions.
+        /// </summary>
+        public static TTarget? MapDynamic<TTarget>(object? source)
+            where TTarget : class, new()
+        {
+            if (source == null) return null;
+            var target = new TTarget();
+            CopyDynamic(source, target);
+            return target;
+        }
+
+        /// <summary>
+        /// Dynamically maps an untyped collection of objects to a List of TTarget using cached compiled expressions.
+        /// </summary>
+        public static List<TTarget> MapListDynamic<TTarget>(IEnumerable<object>? sourceList)
+            where TTarget : class, new()
+        {
+            var result = new List<TTarget>();
+            if (sourceList == null) return result;
+
+            Action<object, object>? cachedCopier = null;
+            Type? lastSourceType = null;
+            var targetType = typeof(TTarget);
+
+            foreach (var item in sourceList)
+            {
+                if (item == null) continue;
+
+                var itemType = item.GetType();
+                if (itemType != lastSourceType || cachedCopier == null)
+                {
+                    lastSourceType = itemType;
+                    var key = (itemType, targetType);
+                    if (!DynamicCopierCache.TryGetValue(key, out cachedCopier))
+                    {
+                        cachedCopier = CreateDynamicCopier(itemType, targetType);
+                        DynamicCopierCache[key] = cachedCopier;
+                    }
+                }
+
+                var target = new TTarget();
+                cachedCopier(item, target);
+                result.Add(target);
+            }
+
+            return result;
+        }
+
         private static Action<TSource, TTarget> CreateCopier<TSource, TTarget>()
         {
             var sourceType = typeof(TSource);
@@ -58,7 +162,7 @@ namespace ZeroPrimitives.Mapping
             var sourceProps = sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
             var targetProps = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-            var blockExpressions = new System.Collections.Generic.List<Expression>();
+            var blockExpressions = new List<Expression>();
 
             foreach (var targetProp in targetProps)
             {
@@ -81,16 +185,25 @@ namespace ZeroPrimitives.Mapping
 
                 if (targetProp.PropertyType == matchSource.PropertyType)
                 {
-                    // Same type: target.Prop = source.Prop
                     var assign = Expression.Assign(Expression.Property(targetParam, targetProp), sourceAccess);
                     blockExpressions.Add(assign);
                 }
                 else if (targetProp.PropertyType.IsAssignableFrom(matchSource.PropertyType))
                 {
-                    // Assignable: target.Prop = (TargetType)source.Prop
                     var cast = Expression.Convert(sourceAccess, targetProp.PropertyType);
                     var assign = Expression.Assign(Expression.Property(targetParam, targetProp), cast);
                     blockExpressions.Add(assign);
+                }
+                else
+                {
+                    var underlyingSource = Nullable.GetUnderlyingType(matchSource.PropertyType) ?? matchSource.PropertyType;
+                    var underlyingTarget = Nullable.GetUnderlyingType(targetProp.PropertyType) ?? targetProp.PropertyType;
+                    if (underlyingSource == underlyingTarget)
+                    {
+                        var cast = Expression.Convert(sourceAccess, targetProp.PropertyType);
+                        var assign = Expression.Assign(Expression.Property(targetParam, targetProp), cast);
+                        blockExpressions.Add(assign);
+                    }
                 }
             }
 
@@ -101,6 +214,68 @@ namespace ZeroPrimitives.Mapping
 
             var block = Expression.Block(blockExpressions);
             var lambda = Expression.Lambda<Action<TSource, TTarget>>(block, sourceParam, targetParam);
+            return lambda.Compile();
+        }
+
+        private static Action<object, object> CreateDynamicCopier(Type sourceType, Type targetType)
+        {
+            var sourceParam = Expression.Parameter(typeof(object), "source");
+            var targetParam = Expression.Parameter(typeof(object), "target");
+
+            var typedSource = Expression.Variable(sourceType, "typedSource");
+            var typedTarget = Expression.Variable(targetType, "typedTarget");
+
+            var sourceProps = sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var targetProps = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var blockExpressions = new List<Expression>();
+            blockExpressions.Add(Expression.Assign(typedSource, Expression.Convert(sourceParam, sourceType)));
+            blockExpressions.Add(Expression.Assign(typedTarget, Expression.Convert(targetParam, targetType)));
+
+            foreach (var targetProp in targetProps)
+            {
+                if (!targetProp.CanWrite) continue;
+
+                PropertyInfo? matchSource = null;
+                for (int i = 0; i < sourceProps.Length; i++)
+                {
+                    if (string.Equals(sourceProps[i].Name, targetProp.Name, StringComparison.OrdinalIgnoreCase) && sourceProps[i].CanRead)
+                    {
+                        matchSource = sourceProps[i];
+                        break;
+                    }
+                }
+
+                if (matchSource == null) continue;
+
+                var sourceAccess = Expression.Property(typedSource, matchSource);
+
+                if (targetProp.PropertyType == matchSource.PropertyType)
+                {
+                    var assign = Expression.Assign(Expression.Property(typedTarget, targetProp), sourceAccess);
+                    blockExpressions.Add(assign);
+                }
+                else if (targetProp.PropertyType.IsAssignableFrom(matchSource.PropertyType))
+                {
+                    var cast = Expression.Convert(sourceAccess, targetProp.PropertyType);
+                    var assign = Expression.Assign(Expression.Property(typedTarget, targetProp), cast);
+                    blockExpressions.Add(assign);
+                }
+                else
+                {
+                    var underlyingSource = Nullable.GetUnderlyingType(matchSource.PropertyType) ?? matchSource.PropertyType;
+                    var underlyingTarget = Nullable.GetUnderlyingType(targetProp.PropertyType) ?? targetProp.PropertyType;
+                    if (underlyingSource == underlyingTarget)
+                    {
+                        var cast = Expression.Convert(sourceAccess, targetProp.PropertyType);
+                        var assign = Expression.Assign(Expression.Property(typedTarget, targetProp), cast);
+                        blockExpressions.Add(assign);
+                    }
+                }
+            }
+
+            var block = Expression.Block(new[] { typedSource, typedTarget }, blockExpressions);
+            var lambda = Expression.Lambda<Action<object, object>>(block, sourceParam, targetParam);
             return lambda.Compile();
         }
     }
--- tests/ZeroPrimitives.Tests/FastMapperTests.cs
+++ tests/ZeroPrimitives.Tests/FastMapperTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 using ZeroPrimitives.Mapping;
 
@@ -11,6 +12,7 @@ namespace ZeroPrimitives.Tests
             public string Name { get; set; } = string.Empty;
             public decimal Price { get; set; }
             public bool IsActive { get; set; }
+            public int? NullableCount { get; set; }
         }
 
         public class SampleEntity
@@ -19,6 +21,7 @@ namespace ZeroPrimitives.Tests
             public string Name { get; set; } = string.Empty;
             public decimal Price { get; set; }
             public bool IsActive { get; set; }
+            public int NullableCount { get; set; }
             public string ExtraField { get; set; } = "Default";
         }
 
@@ -30,7 +33,8 @@ namespace ZeroPrimitives.Tests
                 Id = 101,
                 Name = "Alpha Sensor",
                 Price = 250000m,
-                IsActive = true
+                IsActive = true,
+                NullableCount = 5
             };
 
             var entity = new SampleEntity();
@@ -40,6 +44,7 @@ namespace ZeroPrimitives.Tests
             Assert.Equal("Alpha Sensor", entity.Name);
             Assert.Equal(250000m, entity.Price);
             Assert.True(entity.IsActive);
+            Assert.Equal(5, entity.NullableCount);
             Assert.Equal("Default", entity.ExtraField);
         }
 
@@ -51,7 +56,8 @@ namespace ZeroPrimitives.Tests
                 Id = 202,
                 Name = "Beta Gateway",
                 Price = 5000000m,
-                IsActive = false
+                IsActive = false,
+                NullableCount = 10
             };
 
             var dto = FastMapper.Map<SampleEntity, SampleDto>(entity);
@@ -60,6 +66,57 @@ namespace ZeroPrimitives.Tests
             Assert.Equal("Beta Gateway", dto.Name);
             Assert.Equal(5000000m, dto.Price);
             Assert.False(dto.IsActive);
+            Assert.Equal(10, dto.NullableCount);
+        }
+
+        [Fact]
+        public void MapList_MapsCollectionOfItems()
+        {
+            var entities = new List<SampleEntity>
+            {
+                new SampleEntity { Id = 1, Name = "Item 1", Price = 100m },
+                new SampleEntity { Id = 2, Name = "Item 2", Price = 200m }
+            };
+
+            var dtos = FastMapper.MapList<SampleEntity, SampleDto>(entities);
+
+            Assert.Equal(2, dtos.Count);
+            Assert.Equal(1, dtos[0].Id);
+            Assert.Equal("Item 1", dtos[0].Name);
+            Assert.Equal(2, dtos[1].Id);
+            Assert.Equal("Item 2", dtos[1].Name);
+        }
+
+        [Fact]
+        public void DynamicMapping_MapsUntypedObjectsAndLists()
+        {
+            object rawEntity = new SampleEntity
+            {
+                Id = 303,
+                Name = "Dynamic Device",
+                Price = 99000m,
+                IsActive = true
+            };
+
+            var dto = FastMapper.MapDynamic<SampleDto>(rawEntity);
+            Assert.NotNull(dto);
+            Assert.Equal(303, dto.Id);
+            Assert.Equal("Dynamic Device", dto.Name);
+            Assert.Equal(99000m, dto.Price);
+            Assert.True(dto.IsActive);
+
+            var rawList = new List<object>
+            {
+                new SampleEntity { Id = 10, Name = "Batch 1" },
+                new SampleEntity { Id = 20, Name = "Batch 2" }
+            };
+
+            var dtoList = FastMapper.MapListDynamic<SampleDto>(rawList);
+            Assert.Equal(2, dtoList.Count);
+            Assert.Equal(10, dtoList[0].Id);
+            Assert.Equal("Batch 1", dtoList[0].Name);
+            Assert.Equal(20, dtoList[1].Id);
+            Assert.Equal("Batch 2", dtoList[1].Name);
         }
     }
 }
